6d028a2e88ab5b6921b341a74a7cc698fecd176a
Derek Hanciles <drhanciles@icloud.com>
2018-08-28 16:10:13 -0600

    Test for inserting words correctly is now passing

--- test/node-test.js
+++ test/node-test.js
@@ -3,24 +3,20 @@ import Node from '../lib/node';
 
 describe('NODE', () => {
   let node;
+
   beforeEach(() => {
-    node = new Node('P');
-  }); 
+    node = new Node();
+  })
 
   it('should exist', () => {
     expect(node).to.exist;
-  }); 
-
-  it('should take letter as argument and assign it to the letter property', () => {
-    expect(node.letter).to.equal('P');
-  }); 
+  })
 
   it('should default end to false', () => {
     expect(node.end).to.equal(false)
-  }); 
+  })
 
-  it('should default children to null', () => {
+  it('should default children to an empty object', () => {
     expect(node.children).to.deep.equal({});
-  }); 
-
+  })
 })

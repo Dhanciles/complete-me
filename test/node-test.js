73a230c7d9da1273ce48a6996a832b393d1ec04c
Derek Hanciles <drhanciles@icloud.com>
2018-08-28 13:47:10 -0600

    Updated test and test are now passing

--- test/node-test.js
+++ test/node-test.js
@@ -1,26 +1,26 @@
-import { expect } from 'chai'; 
-import Node from '../lib/node'; 
+import { expect } from 'chai';
+import Node from '../lib/node';
 
-describe('Node', () => {
-  let node; 
-
+describe('NODE', () => {
+  let node;
   beforeEach(() => {
-    node = new Node('z'); 
-  })
-
-  it('should exist', () => {
-    expect(node).to.exist; 
+    node = new Node('P');
   }); 
 
-  it('should take a letter as an argument and assign it as a value to our key', () =>  {
-    expect(node.letter).to.equal('r'); 
+  it('should exist', () => {
+    expect(node).to.exist;
+  }); 
+
+  it('should take letter as argument and assign it to the letter property', () => {
+    expect(node.letter).to.equal('P');
   }); 
 
   it('should default end to false', () => {
-    expect(node.end).to.equal(false); 
+    expect(node.end).to.equal(false)
   }); 
 
-  it('should have children that have a default property of null', () => {
-    expect(node.children).to.equal(null); 
+  it('should default children to null', () => {
+    expect(node.children).to.deep.equal({});
   }); 
+
 })

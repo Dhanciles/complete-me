8afa55b0e2d177b89f08f226bccd3bb229c5be14
Derek Hanciles <drhanciles@icloud.com>
2018-08-29 18:00:03 -0600

    Refactored our function that checks and reassigns node

--- test/trie-test.js
+++ test/trie-test.js
@@ -2,11 +2,12 @@ import { expect } from 'chai';
 import Trie from '../lib/Trie'; 
 import Node from '../lib/Node'; 
 import fs from 'fs';
+require('locus'); 
 
 describe('TRIE', () => {
+  let trie;
   const text = "/usr/share/dict/words";
   const dictionary = fs.readFileSync(text).toString().trim().split('\n')
-  let trie;
 
   beforeEach(() => {
     trie = new Trie();
@@ -38,16 +39,12 @@ describe('TRIE', () => {
     trie.insert('calm'); 
 
     let response = trie.suggest('l'); 
-    console.log(JSON.stringify(trie, null, 4))
+    // console.log(JSON.stringify(trie, null, 4))
     expect(response).to.deep.eq(['lap', 'lost']); 
   }); 
 
   it('should populate with words', () => {
     trie.populate(dictionary); 
-    expect.(trie.totalWords).to.eq(235886)
-  
-
+    expect(trie.totalWords).to.eq(235886); 
   }).timeout(6000)
-
-
 });
